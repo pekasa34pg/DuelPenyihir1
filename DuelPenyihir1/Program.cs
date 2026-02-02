00fa3778c9c3b93e18633e81feaab7e0ac9e6034
perkasa tricahya putra dewa patrikha <perkaspatrikha123@gmail.com>
2026-02-02 11:10:21 +0700

    Menambah Class Wizard, dua object Wizard, dan simulasi battle

--- DuelPenyihir1/Program.cs
+++ DuelPenyihir1/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿wizard wizardA = new wizard("Furina", 20);
+wizard WizardB = new wizard("Hu tao", 20);
+
+Console.WriteLine("Permainan Dimulai...\n");
+wizardA.Showstats();
+WizardB.Showstats();
+
+
+wizardA.attack(WizardB);
+WizardB.attack(wizardA);
+wizardA.attack(WizardB);
+
+Console.WriteLine("Permainan Berakhir...\n");
+wizardA.Showstats();
+WizardB.Showstats();
+public class wizard
+{
+    public string Name;
+    public int Energy;
+    public int Damage;
+
+    // deklarasi constructor
+     public wizard (string name,int damage)
+    {
+        Name = name;
+        Energy = 100;
+        Damage = damage;
+    }
+    
+
+    public void Showstats()
+    {
+        Console.WriteLine("Statistik Wizard");
+        Console.WriteLine($"Nama: {Name},Energi: {Energy} \n");
+    }
+
+
+
+    public void attack(wizard Wizardlawanobj)
+    {
+        //mengurangi energi wizardlawanobj sebesar damage 
+        Wizardlawanobj.Energy -= Damage;
+        Console.WriteLine($"{Name} menyerang {Wizardlawanobj.Name}");
+        Console.WriteLine($"Sisa energi {Wizardlawanobj.Name} adalah {Wizardlawanobj.Energy}");
+       }
+ 
+
+}
